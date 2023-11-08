94bc29bf48bb92855f50d8f99110781f40fdd66d
Super_Rakib <rakibulhassan847@gmail.com>
2023-11-08 18:48:48 +0600

    About us, privacy policy, terms and condition page comple

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,31 +3,32 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <footer className="footer p-10 bg-orange-100 text-base-content">
+        <footer className="footer p-10 bg-slate-950 text-base-content">
   <aside>
-    <img className="w-[30%]" src="https://ebook.projectbd.com/wp-content/uploads/2023/10/hitech-logo.png" alt="" />
-    <p className=" text-black ">HiTech Ltd &copy;2023 <br/>Unlock the Power of Next-Generation Tech</p>
+    <img className="w-[30%]" src="https://ebook.projectbd.com/wp-content/uploads/2023/11/hotel-logo.png" alt="" />
+    <p className=" text-white text-sm font-bold "> LUXURIOUS LIMITED &copy;2023 <br/>
+    <span className=" text-white text-sm font-normal ">Enjoy Your Stay</span></p>
   </aside> 
   <nav>
-    <header className="footer-title text-black">Services</header> 
+    <header className="footer-title text-white">Services</header> 
 
     
-    <Link to="/login"><a className="link link-hover text-black">Login</a></Link> 
-    <Link to="/register"><a className="link link-hover text-black">Register</a></Link> 
-    <Link to="/myCart"><a className="link link-hover text-black">My Cart</a></Link> 
+    <Link to="/login"><a className="link link-hover text-white">Login</a></Link> 
+    <Link to="/register"><a className="link link-hover text-white">Register</a></Link> 
+    {/* <Link to="/myCart"><a className="link link-hover text-white">My Cart</a></Link>  */}
     
   </nav> 
   <nav>
-    <header className="footer-title text-black">Company</header> 
-    <Link to="/about-us"><a className="link link-hover text-black">About us</a></Link> 
+    <header className="footer-title text-white">Company</header> 
+    <Link to="/about-us"><a className="link link-hover text-white">About us</a></Link> 
     {/* <Link to="/login"><a className="link link-hover text-white">Contact</a></Link>  */}
-    <Link to="/addProduct"><a className="link link-hover text-black">Add Product</a></Link> 
+    <Link to="/rooms"><a className="link link-hover text-white">Rooms</a></Link> 
     
   </nav> 
   <nav>
-    <header className="footer-title text-black">Legal</header> 
-    <Link to="/terms"><a className="link link-hover text-black">Terms of use</a></Link> 
-    <Link to="/privacy"><a className="link link-hover text-black">Privacy policy</a></Link> 
+    <header className="footer-title text-white">Legal</header> 
+    <Link to="/terms"><a className="link link-hover text-white">Terms of use</a></Link> 
+    <Link to="/privacy"><a className="link link-hover text-white">Privacy policy</a></Link> 
     
   </nav>
 </footer>
--- src/Components/OptionalPage/TermsAndConditions.jsx
+++ src/Components/OptionalPage/TermsAndConditions.jsx
@@ -7,33 +7,27 @@ const TermsAndConditions = () => {
       <Helmet>
       <title>Terms and Conditions</title>
        </Helmet>
-    <div className="py-8 mt-14 px-12">
-         <h2 className="text-3xl font-semibold mb-4">Terms and Conditions</h2>
-    <p>
-       Welcome to{' '}
-      <Link to="/"><a href="#" className="text-blue-500 hover:underline">
-www.hitech.com
-</a></Link>
-. These terms and conditions outline the rules and regulations for the use of our website and our services. By accessing this website and using our services, you accept these terms and conditions in full.
- </p>
-<p>
-We reserve the right to modify these terms and conditions at any time. You should, therefore, check this page regularly to ensure you are familiar with the current version.
-</p>
-<h3 className="text-xl font-semibold mt-4">Intellectual Property Rights</h3>
- <p>
-   Other than the content you own, under these terms,{' '}
-     <Link to="/"><a href="#" className="text-blue-500 hover:underline">
-     www.hitech.com
-    </a></Link>{' '}
- and/or its licensors own all the intellectual property rights and materials contained on this website.
- </p>
-<h3 className="text-xl font-semibold mt-4">Contact Information</h3>
-  <p> If you have any questions or concerns regarding these terms and conditions, please contact us at{' '}
-      <a href="mailto:info@hitech.com" className="text-blue-500 hover:underline">
-       info@hitech.com.
-  </a>We appreciate your trust in our services, and we look forward to providing you with the best experience.
-</p>
+       <div className="py-8 mt-14 px-12">
+  <h2 className="text-3xl font-semibold mb-4">Terms and Conditions</h2>
+  <p>
+    Welcome to Luxurious Hotel. These terms and conditions outline the rules and regulations for the use of our website and our services. By accessing this website and using our services, you accept these terms and conditions in full.
+  </p>
+  <p>
+    We reserve the right to modify these terms and conditions at any time. You should, therefore, check this page regularly to ensure you are familiar with the current version.
+  </p>
+  <h3 className="text-xl font-semibold mt-4">Intellectual Property Rights</h3>
+  <p>
+    Other than the content you own, under these terms, Luxurious Hotel and/or its licensors own all the intellectual property rights and materials contained on this website.
+  </p>
+  <h3 className="text-xl font-semibold mt-4">Contact Information</h3>
+  <p>
+    If you have any questions or concerns regarding these terms and conditions, please contact us at{' '}
+    <a href="mailto:info@luxurioushotel.com" className="text-orange-500 hover:underline">
+      info@luxurioushotel.com.
+    </a> We appreciate your trust in our services, and we look forward to providing you with the best experience.
+  </p>
 </div>
+
   </div>
     );
 };
